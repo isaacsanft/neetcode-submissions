99435b5f4800f377fafecb110ddc800988c16dd3
Isaac Sanft <112903515+isaacsanft@users.noreply.github.com>
2026-08-28 00:12:44 -0600

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        if len(nums) < 3:
+            return []
+        
+        triplets = set()
+
+        for anchor in range(len(nums) - 2):
+            left = anchor + 1
+            right = len(nums) - 1
+
+            while left < right:
+                three_sum = nums[anchor] + nums[left] + nums[right]
+                if three_sum == 0:
+                    triplets.add((nums[anchor], nums[left], nums[right]))
+                    left += 1
+                    right -= 1
+                elif three_sum < 0:
+                    left += 1
+                else:
+                    right -= 1
+        
+        return list(triplets)
